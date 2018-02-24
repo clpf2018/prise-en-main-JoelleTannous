8f4b9c1adaaaff8946f692cd5c3b5eb0be8683bc
JoelleTannous <joelle.tannous@isae.edu.lb>
2018-02-24 22:54:05 +0200

    tp0

---
+++ tp0/src/tp0/Client.java
@@ -0,0 +1,96 @@
+
+package tp0;
+
+public class Client 
+{
+    private String id;
+    private String prenom;
+    private String nom;
+    private String telephone;
+    private String rue;
+    private String ville;
+    private String etat;
+    private String code;
+    private String pays;
+    private String mail;
+
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+    
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+    
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+    
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+    
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+    
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+    
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }   
+}
